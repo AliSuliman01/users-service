2a05738b340b2fbeaac4411ea9181767debe3512
Ali Suliman <alisulimanq@gmail.com>
2022-09-18 20:20:56 +0300

    fix show user vm

--- app/Http/ViewModels/Users/Users/UserShowVM.php
+++ app/Http/ViewModels/Users/Users/UserShowVM.php
@@ -12,14 +12,13 @@ class UserShowVM implements Arrayable
 
     public function __construct(UserDTO|User $user)
     {
-        if ($user instanceof UserDTO) {
-            if ($user->id !== null)
-                $this->user = User::find($user->id);
-            elseif ($user->email !== null)
-                $this->user = User::where('email', $user->email)->first();
-            else
-                $this->user = User::where('phone_number', $user->phone_number)->first();
-        }
+        if ($user->id !== null)
+            $this->user = User::find($user->id);
+        elseif ($user->email !== null)
+            $this->user = User::where('email', $user->email)->first();
+        else
+            $this->user = User::where('phone_number', $user->phone_number)->first();
+
     }
 
     /**
