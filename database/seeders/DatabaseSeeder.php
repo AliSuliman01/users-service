13e25dc259661e21000650a794c66914948bbdfe
Ali Suliman <alisulimanq@gmail.com>
2022-09-19 22:47:41 +0300

    seed default data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Domain\Categories\Categories\DTO\CategoryDTO;
-use App\Domain\Categories\CategoryTranslation\DTO\CategoryTranslationDTO;
 use App\Domain\Languages\Model\Language;
-use App\Domain\Pages\DTO\PageDTO;
-use App\Domain\Pages\Model\Page;
+use App\Domain\Users\Roles\Model\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,20 +19,7 @@ class DatabaseSeeder extends Seeder
 
         Language::query()->insert($defaultData['languages']);
 
-        foreach ($defaultData['pages'] as $pageData) {
-            $page = new Page(PageDTO::fromRequest($pageData)->toArray());
-            $page->save();
-            foreach ($pageData['categories'] as $categoryData) {
-                $this->handleCategory($categoryData, $page);
-            }
-        }
-    }
+        Role::query()->insert($defaultData['roles']);
 
-    private function handleCategory($categoryData, $belongsTo)
-    {
-        $category = $belongsTo->categories()->create(array_null_filter(CategoryDTO::fromRequest($categoryData)->toArray()));
-        $category->translations()->createMany($categoryData['translations']);
-        foreach ($categoryData['categories'] ?? [] as $subCategoryData)
-            $this->handleCategory($subCategoryData, $category);
     }
 }
